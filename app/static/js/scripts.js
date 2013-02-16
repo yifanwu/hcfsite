b427212584a3b38b37b5c48d1c74b707003ea381
Yuechen Zhao <yuechenzhao1993@gmail.com>
2013-02-16 02:41:09 -0500

    Some aesthetic changes

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -6,6 +6,14 @@ $(document).ready(function() {
     $('#site_register').mouseleave(function() {
 	$('#site_register_top').slideUp('fast');
     });
+
+    $('#site_register').click(function() {
+        window.location = "http://16th-harvardchinaforum.eventdove.com/";
+    });
+
+    $('.ribbon').click(function() {
+        window.location = "http://16th-harvardchinaforum.eventdove.com/";
+    });
     
     $(".scroll_to_anchor").click(function(event){		
 	event.preventDefault();
