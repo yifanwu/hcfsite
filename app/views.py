49efa8697445b6fa651b574644c47552d4f2c292
Yuechen Zhao <yuechenzhao1993@gmail.com>
2013-03-01 11:41:19 -0500

    changing order of teams 2

--- app/views.py
+++ app/views.py
@@ -58,7 +58,7 @@ def view_agenda():
 
 @app.route('/team')
 def view_team():
-    table_team  = Team.query.order_by(Team.name.asc())
+    table_team  = Team.query.all()
     table_group = Group.query.order_by(Group.name.asc())
 
     for group in table_group:
